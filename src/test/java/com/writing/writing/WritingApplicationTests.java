51ba90d7c605580032333b23a9963e913fae27c1
Cuong Nguyen <cuongnvc5802@gmail.com>
2022-05-17 17:26:34 +0700

    Initial commit config project

---
+++ src/test/java/com/writing/writing/WritingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.writing.writing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WritingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
